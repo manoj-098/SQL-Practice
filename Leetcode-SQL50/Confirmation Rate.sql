5ff7600a97d5950fd4aa8d1981316a97bae39dd8
Manoj V <manojvenki098@gmail.com>
2024-12-22 17:40:45 +0530

    Update Confirmation Rate.sql
    https://leetcode.com/problems/confirmation-rate/?envType=study-plan-v2&envId=top-sql-50

--- Leetcode-SQL50/Confirmation Rate.sql
+++ Leetcode-SQL50/Confirmation Rate.sql
@@ -1,5 +1,6 @@
 -- Question : https://leetcode.com/problems/confirmation-rate/?envType=study-plan-v2&envId=top-sql-50
 
+-- Sol[1]
 SELECT s.user_id , ROUND(
     CASE
        WHEN c.action IS NULL THEN 0
@@ -10,3 +11,12 @@ LEFT JOIN Confirmations c
 ON s.user_id=c.user_id
 GROUP BY s.user_id
 ORDER BY confirmation_rate;
+
+
+-- Sol[2]
+
+select s.user_id, round(ifnull(avg(action = 'confirmed'), 0),2) as confirmation_rate from
+Signups s left join Confirmations c
+on s.user_id= c.user_id
+group by s.user_id
+
