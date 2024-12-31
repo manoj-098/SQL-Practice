b9eb24997b0a8bc5967c403ce05efbfdc5b7e9ef
Manoj V <manojvenki098@gmail.com>
2024-12-31 09:23:45 +0530

    Create Customers Who Bought All Products.sql

---
+++ Leetcode-SQL50/Customers Who Bought All Products.sql
@@ -0,0 +1,6 @@
+--Question: https://leetcode.com/problems/customers-who-bought-all-products/?envType=study-plan-v2&envId=top-sql-50
+
+SELECT customer_id
+FROM Customer
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_key)=(SELECT COUNT(*) FROM Product);
