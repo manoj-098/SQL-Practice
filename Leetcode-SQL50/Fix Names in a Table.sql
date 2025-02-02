0d2160253a4272c44c8e535984b33974a4abda26
Manoj V <manojvenki098@gmail.com>
2025-02-02 13:39:16 +0530

    Create Fix Names in a Table.sql

---
+++ Leetcode-SQL50/Fix Names in a Table.sql
@@ -0,0 +1,6 @@
+-- Question: https://leetcode.com/problems/fix-names-in-a-table/?envType=study-plan-v2&envId=top-sql-50
+
+SELECT user_id,
+CONCAT(UPPER(SUBSTRING(name,1,1)),LOWER(SUBSTRING(name,2))) AS name
+FROM Users
+ORDER BY user_id;
