071244b1ca01be8dd01a282e9b6a83926b8467cc
Manoj V <manojvenki098@gmail.com>
2025-02-02 17:28:57 +0530

    Create Consecutive Numbers.sql

---
+++ Leetcode-SQL50/Consecutive Numbers.sql
@@ -0,0 +1,22 @@
+-- Question: https://leetcode.com/problems/consecutive-numbers/description/?envType=study-plan-v2&envId=top-sql-50
+
+#Sol[1]
+SELECT DISTINCT L1.num AS ConsecutiveNums 
+FROM Logs L1
+JOIN Logs L2 ON L1.id=L2.id-1
+JOIN Logs L3 ON L2.id=L3.id-1
+WHERE L1.num=L2.num AND L2.num=L3.num;
+
+
+--Sol[2]
+SELECT DISTINCT L1.num AS ConsecutiveNums 
+FROM Logs L1
+JOIN Logs L2 ON L1.id=L2.id+1
+JOIN Logs L3 ON L2.id=L3.id+1
+WHERE L1.num=L2.num AND L2.num=L3.num;
+
+--Sol[3]
+SELECT DISTINCT L1.num AS ConsecutiveNums 
+FROM Logs L1
+JOIN Logs L2 ON L1.id=L2.id-1 AND L1.num=L2.num
+JOIN Logs L3 ON L2.id=L3.id-1 AND L2.num=L3.num;
