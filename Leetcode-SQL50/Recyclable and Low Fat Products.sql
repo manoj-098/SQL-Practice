aeece2c1242556d57c8182df5d889ff0979367c7
Manoj V <manojvenki098@gmail.com>
2025-02-02 17:14:37 +0530

    Create Recyclable and Low Fat Products.sql

---
+++ Leetcode-SQL50/Recyclable and Low Fat Products.sql
@@ -0,0 +1,4 @@
+-- Question: https://leetcode.com/problems/recyclable-and-low-fat-products/?envType=study-plan-v2&envId=top-sql-50
+
+select product_id from Products
+where low_fats='Y' AND recyclable='Y';
